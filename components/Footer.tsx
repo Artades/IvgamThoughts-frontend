2b30b665b693a6bb7c54b5c4f52be521c75094a1
Artyom Galay <86312709+Artades@users.noreply.github.com>
2023-05-25 17:53:19 +0600

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
 	return (
-		<div className="py-7 mt-11 flex items-start fixed bottom-0">
+		<div className="py-7 mt-11 flex items-start">
 			<p className="text-neutral-500 text-lg">
 				© Ivgam Thoughts 2023. All rights reserved.
 			</p>
